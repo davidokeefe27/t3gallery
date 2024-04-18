ae2154a9f78d245101b0353303bb7a157f0c7a84
David O'Keefe <david.mathew.okeefe@gmail.com>
2024-04-17 21:32:54 -0400

    scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
+import { url } from "inspector";
 import Link from "next/link";
+
+const mockUrls = [
+  "https://utfs.io/f/18dac949-2747-4dbf-8e67-33c2477d8fd9-tlesbb.png",
+  "https://utfs.io/f/4e9d27cb-8670-443a-94bb-ee83b128e558-70kz3p.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-2">{
+        mockImages.map((image) => (
+          <div key={image.id} className="w-1/2 p-4">
+            <img src={image.url} alt="image"/>
+          </div>
+        ))
+      }
+      </div>
       Hello (gallery in progress)
     </main>
   );
